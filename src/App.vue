f0d885144cd7bc1ba96cf3940516a5c92561adea
丁杰承 <2096450462@qq.com>
2022-06-10 18:00:08 +0800

    :sparkles:

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import Footer from './components/Footer.vue'
 onMounted(() => {
   console.log('onMounted')
 })
-const wdnmd = ref<Boolean>(false)
+const wdnmd = ref<boolean>(false)
 </script>
 
 <template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  router.replace('/HelloWorld')
+})
+</script>
 
 <template>
   <h2>Home</h2>
