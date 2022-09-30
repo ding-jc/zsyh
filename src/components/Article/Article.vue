3327372b8743d07f62a0792d5d7b6be7a0fe7420
丁杰承 <2096450462@qq.com>
2022-09-30 17:06:40 +0800

    🎉

--- src/components/Article/Article.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>文章</div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style lang="less" scoped></style>
--- src/views/Header.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>HEADER</div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style></style>
